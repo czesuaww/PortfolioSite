44242fa71344462716f2c374decdd6b2c1831544
Grzegorz_Szwed <greg152295@gmail.com>
2022-03-16 18:35:40 +0100

    added gulp-rename

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const { src, dest } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const autoprefixer = require("gulp-autoprefixer");
 const cssnano = require("gulp-cssnano");
+const rename = require("gulp-rename");
 
 const paths = {
   sass: "./src/sass/**/*.scss",
@@ -9,7 +10,12 @@ const paths = {
 };
 
 function buildStyles(done) {
-  src(paths.sass).pipe(sass().on("error", sass.logError)).pipe(autoprefixer()).pipe(cssnano()).pipe(dest(paths.sassDest));
+  src(paths.sass)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer())
+    .pipe(cssnano())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(dest(paths.sassDest));
   done();
 }
 
